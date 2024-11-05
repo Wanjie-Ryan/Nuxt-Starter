99a269afb97e1372e7a3e5fad0d2cb60170a8597
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-11-05 13:07:30 +0300

    composables

---
+++ composables/useUtils.ts
@@ -0,0 +1,6 @@
+export const useUtils = ()=>{
+    const sayHello = ()=> console.log('Hey mahn, function')
+    return{
+        sayHello
+    }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,24 @@
     <h1>This is the Index Page</h1>
     <img src="@/assets/pic-1.png" alt="image"/>
 </template>
+
+<script>
+import { useUtils } from '~/composables/useUtils';
+export default{
+
+
+    methods:{
+        show(){
+            console.log('Hry there')
+        },
+    },
+    mounted(){
+        const {sayHello} = useUtils()
+        this.show()
+        sayHello()
+    }
+
+}
+
+
+</script>
