b7ec53d3b115c84daf499f990a6e260a9540e7ab
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-11-06 08:07:08 +0300

    useState in nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <Alert />
   <h1>This is the Index Page</h1>
-  <img src="@/assets/pic-1.png" alt="image" />
+  <img src="@/assets/pic-1.png" alt="image" style="width:150px;" />
+  <p>Counter: {{ counter }}</p>
+  <button @click="this.counter++">+</button>
+  <button @click="counter--">-</button>
 </template>
 
 <script>
 import { useUtils } from "~/composables/useUtils";
 export default {
+  data(){
+    return{
+      counter: 0
+    }
+  },
   methods: {
     show() {
       console.log("Hry there");
@@ -15,6 +23,7 @@ export default {
   mounted() {
     console.log("nuxt", this.$nuxt);
     const { sayHello } = useUtils();
+    const counter = useState('counter', ()=> Math.round(Math.random() * 100))
     this.show();
     sayHello();
   },
